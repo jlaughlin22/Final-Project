c2a5d999a80390f8d7e79bb1e7d287daf6358892
jlaughlin22 <46356607+jlaughlin22@users.noreply.github.com>
2019-07-19 03:07:13 -0700

    Fully Commented

--- headers/Doctor.h
+++ headers/Doctor.h
@@ -11,14 +11,22 @@
 #include "Caretaker.h"
 using namespace std;
 
-class Doctor: public Caretaker{
+class Doctor: public Caretaker{//Public Caretaker Doctor is a caretaker
 public:
+
+    /**
+     * Constructor
+     * When made creates a random service time for this instance
+     */
     Doctor(): Caretaker(){
         random_service();
     }
 
+    /**
+     * Random service time for patient (ie how long takes this doctor to treat the patient)
+     */
     void random_service(){
-        service_time = my_num.random_time(20);
+        service_time = my_num.random_time(20);//random value between 1 and 20
     }
 };
 

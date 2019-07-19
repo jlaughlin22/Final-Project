61ebd526fe82f765c0c47d99a8eabfff7c34642c
jlaughlin22 <46356607+jlaughlin22@users.noreply.github.com>
2019-07-19 04:31:04 -0700

    Fully commented

--- headers/binary_sort_person.h
+++ headers/binary_sort_person.h
@@ -15,70 +15,101 @@ using namespace std;
 
 class binary_sort_person{
 private:
-    vector<Person *> binarySort(vector<Person*> vec, int size){
+
+    /**
+     * Takes each value of vector and sorts them by name into an array then reccoppies it back to a vector and returns the vector
+     */
+    vector<Person *> binarySort(vector<Person*> vec, int size){//cited from previous homewok assignments from cs172
         int iPoint = 0;// holding insertion point value
         Person * emptAr[vec.size()];//empty array where sorted values stored
-        emptAr[0] = vec[0];
+        emptAr[0] = vec[0];//first value copy
         for(int i = 1; i < vec.size(); i++){//sorting of each value in array
-            iPoint = binary_search(emptAr, 0, i-1, vec[i]);//i-1 help Scott Griffoth//finds insertion point
+            iPoint = binary_search(emptAr, 0, i-1, vec[i]);//calls binary search
             insert(emptAr, iPoint, vec[i], size);//inserts value
         }
-        vec.clear();
-        for(int i = 0; i < size; i++){
-            vec.push_back(emptAr[i]);
+        vec.clear();//delete contents of vector
+        for(int i = 0; i < size; i++){//increment through sorted array
+            vec.push_back(emptAr[i]);//add items in sorted order back to vector
         }
         return vec;
     }
 
-    int binary_search(Person ** items, int first, int last, Person * target) {
-        int pivot = ( (last - first) / 2 ) + first;
+    /**
+     * returns where vale should go in an array
+     */
+    int binary_search(Person ** items, int first, int last, Person * target) {//cited from previous homewok assignments from cs172
+        int pivot = ( (last - first) / 2 ) + first;//creats pivot point in middle of given range
         if(last < first){ //if pass each other
-            return (first * -1) - 1;
+            return (first * -1) - 1;//return negative index
         } else if((items[pivot]->name == target->name)){// if target found
-            return pivot;
+            return pivot;//return pivot
         }else if(items[pivot]->name > target->name){// if val is greater then target search left
             return binary_search(items, first, pivot-1, target);
         }else if(items[pivot]->name < target->name){// if val is less then target search right
             return binary_search(items, pivot+1, last, target);
         }
     }
-    void insert(Person ** array, int position, Person * value, int arSize){
+
+    /**
+     * Inserts values into an array
+     */
+    void insert(Person ** array, int position, Person * value, int arSize){//cited from previous homewok assignments from cs172
         if(position < 0){//checking if position is negative
             position = (position*-1)-1;//sets position to positive
         }
         shift(array, position, arSize);//shifts values to right
         array[position] = value;//sets ar[position] to value
     }
-    void shift(Person ** array, int position, int arSize){//shifts in right direction
+
+    /**
+     * Shifts the values in an array to the right
+     */
+    void shift(Person ** array, int position, int arSize){//shifts in right direction  //cited from previous homewok assignments from cs172
         //from inclass discussion of code
         for(int i = arSize-1; i > position; i--){//iterating through array
             array[i] = array[i-1];//value equal to value to the left by 1
         }
     }
 
-int binarysearch(const vector<Person*> &items, int first, int last, string target) {
-	int pivot = ( (last - first) / 2 ) + first;
-    if(last < first){ //if pass each other
-        return -1;
-    } else if((items[pivot]->name == target)){// if target found
-        return pivot;
-    }else if(items[pivot]->name > target){// if val is greater then target search left
-        return binarysearch(items, first, pivot-1, target);
-    }else if(items[pivot]->name < target){// if val is less then target search right
-        return binarysearch(items, pivot+1, last, target);
+    /**
+     * Searches for a value 
+     * returns index if found
+     * returns -1 if not found
+     */
+    int binarysearch(const vector<Person*> &items, int first, int last, string target) {
+        int pivot = ( (last - first) / 2 ) + first;
+        if(last < first){ //if pass each other
+            return -1;//not found
+        } else if((items[pivot]->name == target)){// if target found
+            return pivot;//found
+        }else if(items[pivot]->name > target){// if val is greater then target search left
+            return binarysearch(items, first, pivot-1, target);
+        }else if(items[pivot]->name < target){// if val is less then target search right
+            return binarysearch(items, pivot+1, last, target);
+        }
     }
-}
 
 
 public:
+    /**
+     * Empty Constructor
+     */
     binary_sort_person(){}
 
+    /**
+     * Returns a sorted vector of person
+     * Sorts by name
+     */
     vector<Person* > sort_vec(vector<Person*> vec){
-        return binarySort(vec, vec.size());
+        return binarySort(vec, vec.size());//return the sorted vector
     }
 
+    /**
+     * Returns index of person if they are in vector
+     * -1 indicates it is not in vector
+     */
     int binarysearch(const vector<Person*> &items, string target) {
-	    return binarysearch(items, 0, items.size()-1, target);
+	    return binarysearch(items, 0, items.size()-1, target);//return the index
     }
 };
 
